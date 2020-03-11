0a3c5d4af766a5d9139f764816091ed96e9378fe
Dom Vinyard <domvinyard@me.com>
2020-03-11 15:07:31 +0000

    auth basic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 import styled from "styled-components";
 import { RecordRTCPromisesHandler } from "recordrtc";
@@ -6,8 +6,12 @@ import { DBConfig } from "./DBConfig";
 import { initDB, useIndexedDB } from "react-indexed-db";
 import cuid from "cuid";
 import moment from "moment";
+import ScrollableFeed from "react-scrollable-feed";
+import netlifyIdentity from "netlify-identity-widget";
 
 initDB(DBConfig);
+netlifyIdentity.init();
+netlifyIdentity.on("logout", () => window.location.reload());
 
 // She wasn't doing a thing that I could see,
 // except standing there leaning on the balcony railing,
@@ -31,13 +35,42 @@ const App = () => {
   const [recorder, setRecorder] = useState();
   const [recordingStart, setRecordingStart] = useState(false);
   const [timestamp, setTimestamp] = useState(false);
+  const messagesEndRef = useRef(null);
+
+  const user = true; // netlifyIdentity.currentUser();
+
+  // Fetch data on load
+
+  useEffect(() => {
+    const fetchAll = async () => {
+      if (!user) return;
+      const { getAll } = useIndexedDB("recordings");
+      const recordings = await getAll();
+      let newRecordings = [];
+      for (const recording of recordings) {
+        newRecordings.push({
+          ...recording,
+          base64: await readAsDataURLAsync(recording.blob)
+        });
+      }
+      setRecordings(newRecordings);
+      console.log(messagesEndRef);
+      return setIsLoading(false);
+    };
+    fetchAll();
+  }, [user]);
+  useEffect(() => {
+    messagesEndRef.current && messagesEndRef.current.scrollIntoView();
+  }, [isLoading, recordings]);
+
+  // Start and stop (and save) recording
+
   const startRecording = async () => {
     try {
       let stream = await navigator.mediaDevices.getUserMedia({
         audio: true
       });
       console.log({ stream });
-      // alert(JSON.stringify(stream));
       const newRecorder = new RecordRTCPromisesHandler(stream, {
         type: "audio",
         timeSlice: 1000,
@@ -86,29 +119,28 @@ const App = () => {
       console.log(error);
     }
   };
-  useEffect(() => {
-    const fetchAll = async () => {
-      const { getAll } = useIndexedDB("recordings");
-      const recordings = await getAll();
-      let newRecordings = [];
-      for (const recording of recordings) {
-        newRecordings.push({
-          ...recording,
-          base64: await readAsDataURLAsync(recording.blob)
-        });
-      }
-      setRecordings(newRecordings);
-      return setIsLoading(false);
-    };
-    fetchAll();
-  }, []);
 
-  const deleteTodo = async e => {
+  const deleteRecording = async e => {
     const { deleteRecord } = useIndexedDB("recordings");
     const id = e.target.dataset.id;
     setRecordings(recordings.filter(recording => recording.id !== id));
     deleteRecord(id);
   };
+
+  if (!user) {
+    return (
+      <PublicContainer>
+        <h1>memories</h1>
+        <button
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Get started ›
+        </button>
+      </PublicContainer>
+    );
+  }
 
   if (isLoading) {
     return (
@@ -120,31 +152,43 @@ const App = () => {
 
   if (isRecording) {
     return (
-      <div>
-        <BigMic>
-          <i className="fas fa-microphone"></i>{" "}
-          <TimeStamp>
-            {timestamp && recordingStart && timestamp - recordingStart > 0
-              ? moment(timestamp - recordingStart).format("mm:ss")
-              : "00:00"}
-          </TimeStamp>
-        </BigMic>
-        <FixedBottomDark onClick={() => stopRecording()}>
-          End Recording
-        </FixedBottomDark>
-      </div>
+      <Outer className="app">
+        <Inner>
+          <BigMic>
+            <i className="fas fa-microphone"></i>{" "}
+            <TimeStamp>
+              {timestamp && recordingStart && timestamp - recordingStart > 0
+                ? moment(timestamp - recordingStart).format("mm:ss")
+                : "00:00"}
+            </TimeStamp>
+          </BigMic>
+          <FixedBottomDark onClick={() => stopRecording()}>
+            End Recording
+          </FixedBottomDark>
+        </Inner>
+      </Outer>
     );
   }
+
   return (
-    <div className="app">
-      <div className="todo-list">
+    <Outer className="app">
+      <Inner>
+        <FixedTop>
+          <button
+            onClick={() => {
+              console.log("logging out");
+              netlifyIdentity.logout();
+            }}
+          >
+            logout
+          </button>
+        </FixedTop>
         {!recordings || !recordings.length ? (
           <CenterText>no recordings</CenterText>
         ) : (
-          <div>
+          <ScrollableFeed>
             {recordings.filter(Boolean).map((recording, i) => {
               const { id, recordingStart, base64 } = recording;
-              // only show delete button after create API response returns
               return (
                 <RecordingContainer key={id}>
                   <div>{moment(recordingStart).fromNow()}</div>
@@ -152,26 +196,31 @@ const App = () => {
                     <label className="todo">
                       <audio controls src={base64} />
                     </label>
-                    <button data-id={id} onClick={deleteTodo}>
+                    <button data-id={id} onClick={deleteRecording}>
                       delete
                     </button>
                   </div>
                 </RecordingContainer>
               );
             })}
-          </div>
+          </ScrollableFeed>
         )}
+        <div ref={messagesEndRef} />
         <FixedBottom>
           <MicButton onClick={async () => startRecording()}>
             <i className="fas fa-microphone"></i>
           </MicButton>
         </FixedBottom>
-      </div>
-    </div>
+      </Inner>
+    </Outer>
   );
 };
 
-export default App;
+const Inner = styled.div`
+  margin: 0 auto;
+  max-width: 500px;
+`;
+const Outer = styled.div``;
 
 const TimeStamp = styled.div`
   text-align: center;
@@ -183,7 +232,6 @@ const BigMic = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
-  width: 100vw;
   text-align: center;
   .fa-microphone {
     font-size: 12rem;
@@ -194,13 +242,24 @@ const BigMic = styled.div`
 const RecordingContainer = styled.div`
   margin-top: 0.75rem;
 `;
+const FixedTop = styled.div`
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 40px;
+  background: #222;
+  color: white;
+  position: fixed;
+  padding: 8px;
+  text-align: right;
+`;
 const FixedBottom = styled.div`
   position: fixed;
   background: #eee;
   bottom: 0;
   left: 0;
   right: 0;
-  border-top: 2px solid black;
+  border-top: 2px solid aaa;
   text-align: center;
 `;
 
@@ -210,16 +269,16 @@ const FixedBottomDark = styled(FixedBottom)`
   font-weight: bold;
   color: #fff;
   font-size: 2rem;
-  padding: 8px;
+  padding: 18px;
 `;
 
 const MicButton = styled.div`
   background: #ff4d22;
-  width: 40px;
-  height: 40px;
+  width: 60px;
+  height: 60px;
   border-radius: 100%;
-  font-size: 1.5rem;
-  line-height: 40px;
+  font-size: 1.6rem;
+  line-height: 60px;
   margin: 8px auto;
   cursor: pointer;
 `;
@@ -229,3 +288,20 @@ const CenterText = styled.div`
   color: #acacac;
   margin-top: 40vh;
 `;
+
+const PublicContainer = styled.div`
+  background: url(https://img3.goodfon.com/wallpaper/nbig/c/77/starost-skameyka.jpg);
+  height: 100vh;
+  background-size: cover;
+  background-position: bottom right;
+
+  text-align: center;
+  h1 {
+    font-family: "Pacifico", cursive;
+    color: white;
+    font-size: 5rem;
+    margin: 0;
+    padding: 0;
+  }
+`;
+export default App;
