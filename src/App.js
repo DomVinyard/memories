d82fb62218e93745b9888cce858c19790a444f29
Dom Vinyard <domvinyard@me.com>
2020-03-11 17:13:46 +0000

    client

--- src/App.js
+++ src/App.js
@@ -139,40 +139,11 @@ const App = () => {
   const transcribe = async e => {
     const id = e.target.dataset.id;
     const recording = recordings.find(recording => recording.id === id);
-    console.log(recording);
-    const speech = require("@google-cloud/speech");
-    const fs = require("fs");
-
-    // Creates a client
-    const client = new speech.SpeechClient();
-
-    // The name of the audio file to transcribe
-    const fileName = "./resources/audio.raw";
-
-    // Reads a local audio file and converts it to base64
-    const file = fs.readFileSync(fileName);
-    const audioBytes = file.toString("base64");
-
-    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-    const audio = {
-      content: audioBytes
-    };
-    const config = {
-      encoding: "LINEAR16",
-      sampleRateHertz: 16000,
-      languageCode: "en-US"
-    };
-    const request = {
-      audio: audio,
-      config: config
-    };
-
-    // Detects speech in the audio file
-    const [response] = await client.recognize(request);
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join("\n");
-    console.log(`Transcription: ${transcription}`);
+    const response = await fetch("/.netlify/functions/transcribe", {
+      body: recording,
+      method: "POST"
+    });
+    console.log(response);
   };
 
   if (!user) {
