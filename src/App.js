7cfaf0961830e927a71c0d2fed5497fe9223cb86
Dom Vinyard <domvinyard@me.com>
2020-03-11 12:38:36 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
-import ls from "local-storage";
-import api from "./utils/api";
-import isLocalHost from "./utils/isLocalHost";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import styled from "styled-components";
 import { RecordRTCPromisesHandler } from "recordrtc";
@@ -29,7 +26,6 @@ function readAsDataURLAsync(blob) {
 
 const App = () => {
   const [recordings, setRecordings] = useState([]);
-  const [todos, setTodos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isRecording, setIsRecording] = useState(false);
   const [recorder, setRecorder] = useState();
@@ -84,7 +80,6 @@ const App = () => {
     const fetchAll = async () => {
       const { getAll } = useIndexedDB("recordings");
       const recordings = await getAll();
-      console.log(recordings);
       let newRecordings = [];
       for (const recording of recordings) {
         newRecordings.push({
@@ -97,50 +92,6 @@ const App = () => {
     };
     fetchAll();
   }, []);
-  const saveRecording = async recordingValue => {
-    if (!recordingValue) {
-      alert("no recording");
-      return false;
-    }
-    const recordingInfo = {
-      ...recordingValue,
-      completed: false
-    };
-
-    var reader = new FileReader();
-    reader.readAsDataURL(recordingValue.blob);
-    reader.onloadend = async () => {
-      try {
-        const base64data = reader.result;
-        // Optimistically add todo to UI
-        const optimisticRecordingsState = [
-          ...todos,
-          {
-            data: { ...recordingInfo, base64data },
-            ts: new Date().getTime() * 10000
-          }
-        ];
-        console.log({ optimisticRecordingsState });
-        setTodos(optimisticRecordingsState);
-        const newItem = await api.create(recordingInfo);
-        const newId = getTodoId(newItem);
-        ls(newId, base64data);
-        newItem.data.base64data = base64data;
-        // remove temporaryValue from state and persist API response
-        const persistedState = removeOptimisticTodo(todos).concat(newItem);
-        // Set persisted value to state
-        setTodos(persistedState);
-        console.log({ persistedState });
-      } catch (e) {
-        console.log("An API error occurred", e);
-        const revertedState = removeOptimisticTodo(todos);
-        // // Reset to original state
-        setTodos(revertedState);
-      }
-    };
-
-    // Make API request to create new todo
-  };
 
   const deleteTodo = async e => {
     console.log("delete me");
@@ -148,55 +99,14 @@ const App = () => {
     const id = e.target.dataset.id;
     setRecordings(recordings.filter(recording => recording.id !== id));
     deleteRecord(id);
-    return;
-    const todoId = e.target.dataset.id;
-
-    // Optimistically remove todo from UI
-    const filteredTodos = todos.reduce(
-      (acc, current) => {
-        const currentId = getTodoId(current);
-        if (currentId === todoId) {
-          // save item being removed for rollback
-          acc.rollbackTodo = current;
-          return acc;
-        }
-        // filter deleted todo out of the todos list
-        acc.optimisticState = acc.optimisticState.concat(current);
-        return acc;
-      },
-      {
-        rollbackTodo: {},
-        optimisticState: []
-      }
-    );
-
-    setTodos(filteredTodos.optimisticState);
-
-    // Make API request to delete todo
-    try {
-      await api.delete(todoId);
-      ls.remove(todoId);
-      console.log(`deleted todo id ${todoId}`);
-    } catch (e) {
-      console.log(`There was an error removing ${todoId}`, e);
-      // Add item removed back to list
-      setTodos(
-        filteredTodos.optimisticState.concat(filteredTodos.rollbackTodo)
-      );
-    }
   };
 
   if (isLoading) {
     return (
       <CenterText>
-        <i class="fas fa-spinner fa-pulse"></i>
+        <i className="fas fa-spinner fa-pulse"></i>
       </CenterText>
     );
-  }
-
-  if (!recordings || !recordings.length) {
-    // Loading State here
-    return <CenterText>no recordings</CenterText>;
   }
 
   if (isRecording) {
@@ -212,54 +122,39 @@ const App = () => {
   return (
     <div className="app">
       <div className="todo-list">
-        <div>
-          {recordings.filter(Boolean).map((recording, i) => {
-            const { id, recordingStart, base64 } = recording;
-            console.log({ recording });
-            // only show delete button after create API response returns
-            return (
-              <RecordingContainer>
-                <div>{moment(recordingStart).fromNow()}</div>
-                <div key={id} className="todo-item">
-                  <label className="todo">
-                    <audio controls src={base64} />
-                  </label>
-                  <button data-id={id} onClick={deleteTodo}>
-                    delete
-                  </button>
-                </div>
-              </RecordingContainer>
-            );
-          })}
-        </div>
-
+        {!recordings || !recordings.length ? (
+          <CenterText>no recordings</CenterText>
+        ) : (
+          <div>
+            {recordings.filter(Boolean).map((recording, i) => {
+              const { id, recordingStart, base64 } = recording;
+              console.log({ recording });
+              // only show delete button after create API response returns
+              return (
+                <RecordingContainer key={id}>
+                  <div>{moment(recordingStart).fromNow()}</div>
+                  <div key={id} className="todo-item">
+                    <label className="todo">
+                      <audio controls src={base64} />
+                    </label>
+                    <button data-id={id} onClick={deleteTodo}>
+                      delete
+                    </button>
+                  </div>
+                </RecordingContainer>
+              );
+            })}
+          </div>
+        )}
         <FixedBottom>
-          <MicButton
-            onClick={async () => {
-              startRecording();
-            }}
-          >
-            <i class="fas fa-microphone"></i>
+          <MicButton onClick={async () => startRecording()}>
+            <i className="fas fa-microphone"></i>
           </MicButton>
         </FixedBottom>
       </div>
     </div>
   );
 };
-
-function removeOptimisticTodo(todos) {
-  // return all 'real' todos
-  return todos.filter(todo => {
-    return todo.ref;
-  });
-}
-
-function getTodoId(todo) {
-  if (!todo.ref) {
-    return null;
-  }
-  return todo.ref["@ref"].id;
-}
 
 export default App;
 
